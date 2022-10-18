0a352326d1dbdcd76eba2d9c94c3ca745a18f37a
Sam Nilsson <sam64114208@gmail.com>
2022-10-18 14:37:06 +0200

    Made idle animation

--- Source/UntitledFishGame/PlayerStuff/FishPlayerAnim.cpp
+++ Source/UntitledFishGame/PlayerStuff/FishPlayerAnim.cpp
@@ -5,8 +5,10 @@ void UFishPlayerAnim::Idle()
 {
 	const float TotalTime = GetWorld()->GetTimeSeconds();
 	const float AnimTime = IdleCurves.AnimTime;
-	const float Time = TotalTime - truncf(TotalTime / AnimTime) * AnimTime;
+	const float Time = TotalTime - static_cast<int>(TotalTime / AnimTime) * AnimTime;
 	HeadLocation = IdleCurves.HeadCurve->GetVectorValue(Time);
+	HandRLocation = IdleCurves.HandRCurve->GetVectorValue(Time);
+	HandLLocation = IdleCurves.HandLCurve->GetVectorValue(Time);
 }
 
 void UFishPlayerAnim::Walk()
